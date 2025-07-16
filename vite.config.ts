71cefa81951a535a52c559366c7258804190bb69
Minh Do <131603843+minhdnn@users.noreply.github.com>
2025-07-16 08:53:45 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,37 @@
-import path from 'path';
+import { fileURLToPath, URL } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
+  const env = loadEnv(mode, '.', '');
+  return {
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('.', import.meta.url)),
+      }
+    },
+    build: {
+      rollupOptions: {
+        onwarn(warning, warn) {
+          if (warning.code === 'MODULE_LEVEL_DIRECTIVE' && warning.message.includes(`"use client"`)) {
+            return;
+          }
+          warn(warning);
+        },
+        output: {
+          entryFileNames: 'app.js',             // JS chính
+          chunkFileNames: 'chunk-[name].js',    // JS phụ nếu có
+          assetFileNames: ({ name }) => {
+            if (name && name.endsWith('.css')) {
+              return 'style.css';               // CSS
+            }
+            return '[name][extname]';           // asset khác (ảnh, font)
+          }
         }
       }
-    };
+    }
+  };
 });
